1d5a87560f6b5af7a11a474221b57d1f6f1bb580
QFabius <75182785+QFabius@users.noreply.github.com>
2023-03-31 14:51:15 +0200

    Add interaction with board

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
 nrows = 6
 ncols = 7
+pieces = ['X', 'O']
 
-def create_board(nrows, ncols):
+def create_board():
   board = []
   for row in range(nrows):
     board_row = []
     for col in range(ncols):
       board_row.append(' ')
     board.append(board_row)
+  # print_board(board)
   return board
 
 def print_board(board):
@@ -28,5 +30,34 @@ def print_board(board):
     print(row_str)
     print(bound_str)
 
-board = create_board(nrows, ncols)
+def drop(board, col, piece):
+  if col < 1 or col > ncols:
+    print("Enter valid column.")
+    return board
+  if piece not in pieces:
+    print("Enter valid piece: X or O.")
+    return board
+  col -= 1
+  for row in range(len(board)-1,-1,-1):
+    if board[row][col] == ' ':
+      board[row][col] = piece
+      break
+    elif row == 0:
+      print("Column full. Choose other column.")
+  # print_board(board)
+  return board
+
+board = create_board()
+board = drop(board, 1, 'X')
+board = drop(board, 1, 'O')
+board = drop(board, 2, 'X')
+board = drop(board, 2, 'O')
+board = drop(board, 2, 'X')
+board = drop(board, 2, 'O')
+board = drop(board, 2, 'X')
+board = drop(board, 2, 'O')
+board = drop(board, 2, 'X') # Full column
+board = drop(board, 8, 'X') # Out of bounds
+board = drop(board, 2, 'T') # Wrong piece
+board = drop(board, 7, 'X')
 print_board(board)
